c61384103841b373aa72ebc7b5c2e4abb6889f0a
Parker-Corbitt <parkercorbitt36@gmail.com>
2023-09-11 16:39:17 -0400

    added last example

---
+++ Error_Examples/program4.cc
@@ -0,0 +1,26 @@
+/**
+ *        @file: program4.cc
+ *      @author: Parker Corbitt
+ *        @date: September 11, 2023
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+/// function prototypes
+
+int main(int argc, char const *argv[])
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        int x = 0;
+        x++;
+    }
+    cout << x << endl;
+    cout << i;
+    return 0;
+} // main
