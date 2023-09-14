c8b79e761ad440f95127086f76f3b5aa1adb21a9
Parker-Corbitt <parkercorbitt36@gmail.com>
2023-09-14 08:31:52 -0400

    examples filled out

---
+++ Error_Examples/program5.cc
@@ -0,0 +1,24 @@
+/**
+ *        @file: program5.cc
+ *      @author: Parker Corbitt
+ *        @date: September 13, 2023
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    int j = 0;
+    for(int i = 0; i < 2; ++i)
+    {
+        cout << i << endl;
+        cout << "hello" << endl;
+    }
+    return 0;
+}// main
